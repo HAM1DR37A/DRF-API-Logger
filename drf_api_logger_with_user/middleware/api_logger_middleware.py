16e09bb26308b1d63f1cb1c00695f2e5363bdc3e
hamid <pandora10mk@gmail.com>
2022-06-23 15:15:47 +0430

    Log non application/json requests

--- drf_api_logger_with_user/middleware/api_logger_middleware.py
+++ drf_api_logger_with_user/middleware/api_logger_middleware.py
@@ -101,7 +101,9 @@ class APILoggerMiddleware:
             if len(self.DRF_API_LOGGER_METHODS) > 0 and method not in self.DRF_API_LOGGER_METHODS:
                 return self.get_response(request)
 
-            if response.get('content-type') in ('application/json', 'application/vnd.api+json',):
+            if not response.get('content-type') in ('application/json', 'application/vnd.api+json',):
+                response_body = {}
+            else:
                 if getattr(response, 'streaming', False):
                     response_body = '** Streaming **'
                 else:
@@ -109,40 +111,39 @@ class APILoggerMiddleware:
                         response_body = json.loads(response.content.decode())
                     else:
                         response_body = json.loads(response.content)
-                if self.DRF_API_LOGGER_PATH_TYPE == 'ABSOLUTE':
-                    api = request.build_absolute_uri()
-                elif self.DRF_API_LOGGER_PATH_TYPE == 'FULL_PATH':
-                    api = request.get_full_path()
-                elif self.DRF_API_LOGGER_PATH_TYPE == 'RAW_URI':
-                    api = request.get_raw_uri()
-                else:
-                    api = request.build_absolute_uri()
-
-                data = dict(
-                    api=mask_sensitive_data_url(api),
-                    headers=mask_sensitive_data(headers),
-                    body=mask_sensitive_data(request_data),
-                    method=method,
-                    client_ip_address=get_client_ip(request),
-                    response=mask_sensitive_data(response_body),
-                    status_code=response.status_code,
-                    execution_time=time.time() - start_time,
-                    added_on=timezone.now(),
-                    user_id=request.user.id if (request.user and request.user.is_authenticated) else None,
-                    response_size=get_size(response.content)
-                )
-                if self.DRF_API_LOGGER_DATABASE:
-                    if LOGGER_THREAD:
-                        d = data.copy()
-                        d['headers'] = json.dumps(d['headers'], indent=4)
-                        if request_data:
-                            d['body'] = json.dumps(d['body'], indent=4)
-                        d['response'] = json.dumps(d['response'], indent=4)
-                        LOGGER_THREAD.put_log_data(data=d)
-                if self.DRF_API_LOGGER_SIGNAL:
-                    API_LOGGER_SIGNAL.listen(**data)
+            if self.DRF_API_LOGGER_PATH_TYPE == 'ABSOLUTE':
+                api = request.build_absolute_uri()
+            elif self.DRF_API_LOGGER_PATH_TYPE == 'FULL_PATH':
+                api = request.get_full_path()
+            elif self.DRF_API_LOGGER_PATH_TYPE == 'RAW_URI':
+                api = request.get_raw_uri()
             else:
-                return response
+                api = request.build_absolute_uri()
+
+            data = dict(
+                api=mask_sensitive_data_url(api),
+                headers=mask_sensitive_data(headers),
+                body=mask_sensitive_data(request_data),
+                method=method,
+                client_ip_address=get_client_ip(request),
+                response=mask_sensitive_data(response_body),
+                status_code=response.status_code,
+                execution_time=time.time() - start_time,
+                added_on=timezone.now(),
+                user_id=request.user.id if (request.user and request.user.is_authenticated) else None,
+                response_size=get_size(response.content)
+            )
+            if self.DRF_API_LOGGER_DATABASE:
+                if LOGGER_THREAD:
+                    d = data.copy()
+                    d['headers'] = json.dumps(d['headers'], indent=4)
+                    if request_data:
+                        d['body'] = json.dumps(d['body'], indent=4)
+                    d['response'] = json.dumps(d['response'], indent=4)
+                    LOGGER_THREAD.put_log_data(data=d)
+            if self.DRF_API_LOGGER_SIGNAL:
+                API_LOGGER_SIGNAL.listen(**data)
+
         else:
             response = self.get_response(request)
         return response
